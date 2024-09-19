00062981217642bd6fe32daeb5b8003d201bf9f3
Vecoo <vecoo@list.ru>
2024-09-20 02:29:33 +1100

    1.20.1.

---
+++ forge201/src/main/java/com/vecoo/extralib/ExtraLib.java
@@ -0,0 +1,42 @@
+package com.vecoo.extralib;
+
+import net.minecraft.server.MinecraftServer;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(ExtraLib.MOD_ID)
+public class ExtraLib {
+    public static final String MOD_ID = "extralib";
+    private static final Logger LOGGER = LogManager.getLogger("ExtraLib");
+
+    private static ExtraLib instance;
+
+    private MinecraftServer server;
+
+    public ExtraLib() {
+        instance = this;
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+        server = event.getServer();
+    }
+
+    public static ExtraLib getInstance() {
+        return instance;
+    }
+
+    public static Logger getLogger() {
+        return LOGGER;
+    }
+
+    public MinecraftServer getServer() {
+        return instance.server;
+    }
+}
