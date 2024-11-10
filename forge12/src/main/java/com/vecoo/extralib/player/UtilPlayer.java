d4ec2ac1431b194a9333dc367dc55e3a0a291f50
Vecoo <vecoo@list.ru>
2024-11-11 10:11:36 +1100

    1.2.2 - Fix UsernameCache.

--- forge12/src/main/java/com/vecoo/extralib/player/UtilPlayer.java
+++ forge12/src/main/java/com/vecoo/extralib/player/UtilPlayer.java
@@ -7,17 +7,11 @@ import java.util.UUID;
 import java.util.stream.Collectors;
 
 public class UtilPlayer {
-    private static final Map<String, UUID> username = UtilPlayer.invertMap(UsernameCache.getMap());
-
-    public static <K, V> Map<V, K> invertMap(Map<K, V> map) {
-        return map.entrySet().stream().collect(Collectors.toMap(Map.Entry::getValue, Map.Entry::getKey));
-    }
-
     public static UUID getUUID(String player) {
-        return username.get(player);
+        return UsernameCache.getMap().entrySet().stream().collect(Collectors.toMap(Map.Entry::getValue, Map.Entry::getKey)).get(player);
     }
 
     public static boolean hasUUID(String player) {
-        return username.containsKey(player);
+        return UsernameCache.getMap().entrySet().stream().collect(Collectors.toMap(Map.Entry::getValue, Map.Entry::getKey)).containsKey(player);
     }
 }
