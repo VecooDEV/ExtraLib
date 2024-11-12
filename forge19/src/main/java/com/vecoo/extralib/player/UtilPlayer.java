7c37f6ea17dc7faa79528b8c3d87a40853c5da6e
Vecoo <vecoo@list.ru>
2024-11-13 10:40:39 +1100

    Support 1.19.2.

---
+++ forge19/src/main/java/com/vecoo/extralib/player/UtilPlayer.java
@@ -0,0 +1,17 @@
+package com.vecoo.extralib.player;
+
+import net.minecraftforge.common.UsernameCache;
+
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+public class UtilPlayer {
+    public static UUID getUUID(String player) {
+        return UsernameCache.getMap().entrySet().stream().collect(Collectors.toMap(Map.Entry::getValue, Map.Entry::getKey)).get(player);
+    }
+
+    public static boolean hasUUID(String player) {
+        return UsernameCache.getMap().entrySet().stream().collect(Collectors.toMap(Map.Entry::getValue, Map.Entry::getKey)).containsKey(player);
+    }
+}
---
+++ forge19/src/main/java/com/vecoo/extralib/storage/player/LibPlayerFactory.java
@@ -0,0 +1,24 @@
+package com.vecoo.extralib.storage.player;
+
+import com.vecoo.extralib.ExtraLib;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public class LibPlayerFactory {
+    public static boolean hasCommandCooldown(UUID playerUUID, String command) {
+        return ExtraLib.getInstance().getPlayerProvider().getPlayerStorage(playerUUID).hasKeyCooldown(command);
+    }
+
+    public static HashMap<String, Long> getCommandCooldown(UUID playerUUID) {
+        return ExtraLib.getInstance().getPlayerProvider().getPlayerStorage(playerUUID).getKeyCooldown();
+    }
+
+    public static void addCommandCooldown(UUID playerUUID, String command, long time) {
+        ExtraLib.getInstance().getPlayerProvider().getPlayerStorage(playerUUID).addKeyCooldown(command, time);
+    }
+
+    public static void removeCommandCooldown(UUID playerUUID, String command) {
+        ExtraLib.getInstance().getPlayerProvider().getPlayerStorage(playerUUID).removeKeyCooldown(command);
+    }
+}
