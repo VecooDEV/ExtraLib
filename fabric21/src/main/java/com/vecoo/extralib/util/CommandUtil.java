5fab3395e4fa380db335c585e633557bd7f32a0b
Vecoo <vecoo@list.ru>
2026-04-06 14:45:36 +1100

    remove ServerUtil class

--- fabric21/src/main/java/com/vecoo/extralib/util/CommandUtil.java
+++ fabric21/src/main/java/com/vecoo/extralib/util/CommandUtil.java
@@ -1,13 +1,30 @@
 package com.vecoo.extralib.util;
 
 import com.mojang.brigadier.suggestion.SuggestionProvider;
+import com.vecoo.extralib.ExtraLib;
 import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.server.MinecraftServer;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
 
 public final class CommandUtil {
     private CommandUtil() {
+    }
+
+    /**
+     * Executes a server command as if it was run by the console.
+     * <p>
+     * This method creates a command source stack for the server console and performs the
+     * specified command. It does not require a player and runs with full permissions.
+     * </p>
+     *
+     * @param command the command to execute, without a leading slash
+     */
+    public static void executeCommand(@NotNull String command) {
+        MinecraftServer server = ExtraLib.getInstance().getServer();
+
+        server.getCommands().performPrefixedCommand(server.createCommandSourceStack(), command);
     }
 
     /**
